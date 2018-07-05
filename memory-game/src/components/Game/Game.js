10904089be783ec455d1c53af63f18a503de725b
kerrickwoyshner <kerrick.woyshner@gmail.com>
2018-07-05 11:16:09 -0400

    initial Game logic

---
+++ memory-game/src/components/Game/Game.js
@@ -0,0 +1,70 @@
+import React, {Component} from "react";
+import Nav from "../Nav";
+import Header from "../Header";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from "../Footer";
+import data from "../data.json";
+
+class Game extends Component {
+    state = {
+        data,
+        score: 0,
+        topScore: 0
+    };
+
+    componentDidMount() {
+        this.setState({ data: this.shuffleData(this.state.data) });
+    }
+
+    handleCorrectGuess = newData => {
+        const { topScore, score } = this.state;
+        const newScore = score + 1;
+        const newTopScore = newScore > topScore ? newScore : topScore;
+        this.setState({
+            data: this.shuffleData(newData),
+            score: newScore,
+            topScore: newTopScore
+        });
+    };
+
+    handleIncorrectGuess = data => {
+        this.setState({
+            data: this.resetData(data),
+            score: 0
+        });
+    };
+
+    resetData = data => {
+        const resetData = data.map(item => ({ ...item, clicked: false }));
+        return this.shuffleData(resetData);
+    };
+
+    shuffleData = data => {
+        let i = data.length - 1;
+        while (i > 0) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = data[i];
+            data[i] = data[j];
+            data[j] = data[i];
+            data[j] = temp;
+            i--;
+        }
+        return data;
+    };
+
+    handleItemClick = id => {
+        letguessedCorrectly = false;
+        const newData = this.state.data.map(item => {
+            const newItem = { ...item };
+            if (newItem.id === id ) {
+                if (!newItem.clicked) {
+                    newItem.clicked = true;
+                    guessedCorrectly = true;
+                }
+            }
+            return newItem;
+        });
+        guessedCorrectly
+    }
+}
